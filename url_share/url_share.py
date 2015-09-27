c1fecd28b751cdc1779c3090247247eb06ce770b
k <k@meh>
2015-09-27 16:53:36 +0100

    Nailed it, damn url forms and empty values.

--- url_share/url_share.py
+++ url_share/url_share.py
@@ -10,23 +10,27 @@ invoked e.g. from a terminal mail client like mutt, and open the URL on
 another computer.
 """
 
+import argparse
+
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
+urls = list()
+
 
 def store_url(req):
-    global urls
     _url_data = req.args
     _form_data = req.form
     if _form_data:
         # as application/x-www-form-urlencoded
         for _k, _v in _form_data.items():
-            if not _v:
-                continue
-            if _k == 'url':
+            if _k == 'url' and _v:
                 urls.append(_v)
-            else:
+            elif _k != 'url' and not _v:
                 urls.append(_k)
+            else:
+                # empty url
+                continue
     if _url_data:
         _url = _url_data.get('url')
         if _url:
@@ -49,12 +53,14 @@ def return_all():
 @app.route('/clean', methods=['POST'])
 def cleanup():
     global urls
-    urls.clear()
+    urls = []
     return return_all()
 
 
 if __name__ == '__main__':
-    global urls
-    urls = list()
-    app.debug = True
+    p = argparse.ArgumentParser()
+    p.add_argument('-d', '--debug', action='store_true')
+    args = p.parse_args()
+    if args.debug:
+        app.debug = True
     app.run()
