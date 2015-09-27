d777597d4f74a4db4533a335df05962a141b7dec
k <k@meh>
2015-09-27 16:42:36 +0100

    Better handling of forms.

--- url_share/url_share.py
+++ url_share/url_share.py
@@ -17,32 +17,44 @@ app = Flask(__name__)
 def store_url(req):
     global urls
     _url_data = req.args
-    _post_data = req.form
-    if _post_data:
-        # sent from curl
-        for item in _post_data.keys():
-            urls.add(item)
+    _form_data = req.form
+    if _form_data:
+        # as application/x-www-form-urlencoded
+        for _k, _v in _form_data.items():
+            if not _v:
+                continue
+            if _k == 'url':
+                urls.append(_v)
+            else:
+                urls.append(_k)
     if _url_data:
         _url = _url_data.get('url')
-        urls.add(_url)
+        if _url:
+            urls.append(_url)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def share_url():
     if request.method == 'POST':
         store_url(request)
-    return render_template('shared_urls.html', urls=urls)
+
+    return return_all()
+
+
+def return_all():
+    # return the list, reversed
+    return render_template('shared_urls.html', urls=urls[::-1])
 
 
 @app.route('/clean', methods=['POST'])
 def cleanup():
     global urls
     urls.clear()
-    return render_template('shared_urls.html', urls=urls)
+    return return_all()
 
 
 if __name__ == '__main__':
     global urls
-    urls = set()
-    # app.debug = True
+    urls = list()
+    app.debug = True
     app.run()
